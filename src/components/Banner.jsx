20e886cfaa1aa0be0541f31e8f1c4ea61f9965e9
Muzaffarhussain0786 <muzaffarhussain07865@gmail.com>
2025-07-11 12:08:37 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ function Banner() {
             <section class="specials-section">
                 <h2>Today's Specials</h2>
                 <div class="specials-grid">
-                    <OfferCard title="Channa Puri" description="Warm, Fluffy, Flavorful. Your Channa Puri Craving, Satisfied." price={21} imageUrl="https://i.pinimg.com/736x/19/2d/72/192d722849ed087bf9442e978544f954.jpg" btnText="View Details" />
+                    <OfferCard title="Channa Puri" description="Warm, Fluffy, Flavorful. Your Channa Puri Craving, Satisfied." price={55} imageUrl="https://i.pinimg.com/736x/19/2d/72/192d722849ed087bf9442e978544f954.jpg" btnText="View Details" />
                     <OfferCard title="Chhole Bhature" description="Tasty, delicious and what not" imageUrl="https://th.bing.com/th/id/OIP.m_c2Un-TR2tCfJ6mcWmM8wHaE8?w=290&h=193&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" price={70} btnText="Order Now" />
                     <OfferCard title="Rajma Chawal" description="Tasty, delicious and what not" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrZZYV9Eu13fxu8qXT98ovX6SrG3hGOtpqPw&s" price={80} />
                     {/* <OfferCard title="Chhole Bhature" description="Tasty, delicious and what not" imageUrl="https://th.bing.com/th/id/OIP.m_c2Un-TR2tCfJ6mcWmM8wHaE8?w=290&h=193&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" price={150} />
